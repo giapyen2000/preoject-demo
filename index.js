b17c44eb04f9f05708db5f5fe9e5d9dc4c075cdc
yenkika <giapthiyenvtvybg@gmail.com>
2021-05-21 12:05:51 -0400

    add  router demo

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
+app.get('/', function (req, res) {
+    res.send('Hello World')
+});
+
+app.get('/demo', function (req, res) {
+    return res.json('Hello World')
+})
+     
 app.listen(3000, () => {
     console.log('starting')
 })
